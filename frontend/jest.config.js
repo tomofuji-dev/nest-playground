80768227bc74afdd4d43dfca93c5d04332bbe331
tfuji <tomofuji.dev@gmail.com>
2023-08-13 18:56:18 +0900

    jest config

---
+++ frontend/jest.config.js
@@ -0,0 +1,16 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
+};
+
+module.exports = createJestConfig(customJestConfig);
