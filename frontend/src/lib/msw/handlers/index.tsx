fcf4958630a1ef0acd357f5ed11caa1eadcbceec
tfuji <tomofuji.dev@gmail.com>
2023-08-13 18:58:12 +0900

    ctx.delay

--- frontend/src/lib/msw/handlers/index.tsx
+++ frontend/src/lib/msw/handlers/index.tsx
@@ -5,6 +5,7 @@ import { API_URL } from '@/config/constants';
 export const handlers = [
   rest.get(`${API_URL}/health`, (req, res, ctx) => {
     return res(
+      ctx.delay(1000),
       ctx.status(200),
       ctx.text('OK')
     );
