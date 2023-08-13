b0965f8b25eb4b5278a06f4eab7589f2372a4c71
tfuji <tomofuji.dev@gmail.com>
2023-08-13 19:41:31 +0900

    msw-server, browserの分離

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,10 +2,10 @@ import { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
 
 import { API_MOCKING } from '@/config/constants';
-import { MSWWrapperProps } from '@/lib/msw';
+import { MSWWrapperProps } from '@/lib/msw/MSWWrapper';
 
 const MSWWrapper = dynamic<MSWWrapperProps>(() =>
-  import('@/lib/msw').then(({ MSWWrapper }) => MSWWrapper)
+  import('@/lib/msw/MSWWrapper').then(({ MSWWrapper }) => MSWWrapper)
 );
 
 const App = ({ Component, pageProps }: AppProps) => {
